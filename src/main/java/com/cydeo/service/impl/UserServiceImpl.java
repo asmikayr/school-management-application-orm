f628b43c75b4b7e53e820b4be997a762e588112e
Dao <dao.dekyi@gmail.com>
2024-06-12 13:36:16 -0700

    Commit UserStory 1 - UserService, UserServiceImpl

---
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -0,0 +1,58 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+import com.cydeo.mapper.UserMapper;
+import com.cydeo.repository.UserRepository;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.antlr.v4.runtime.tree.xpath.XPath.findAll;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public List<UserDTO> listAllUsers() {
+        List<User> userList = userRepository.findAllByIsDeletedOrderByFirstNameDesc(false);
+        return userList.stream().map(userMapper::convertToDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDTO findByUserName(String username) {
+        User user = userRepository.findByUserNameAndIsDeleted(username, false);
+        return userMapper.convertToDto(user);
+    }
+
+    @Override
+    public void save(UserDTO user) {
+        userRepository.save(userMapper.convertToEntity(user));
+    }
+
+    @Override
+    public UserDTO update(UserDTO user) {
+        User user1 = userRepository.findByUserNameAndIsDeleted(user.getUserName(), false);
+        User convertedUser = userMapper.convertToEntity(user);
+        convertedUser.setId(user1.getId());
+        userRepository.save(convertedUser);
+        return findByUserName(user.getUserName());
+    }
+
+    @Override
+    public void delete(String username) {
+
+    }
+
+
+}
