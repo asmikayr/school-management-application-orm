334dd66963992b6d0da2cfd7aacf49ba01c295c8
Dao <dao.dekyi@gmail.com>
2024-06-12 13:34:18 -0700

    Commit UserStory 1 - RoleService, RoleServiceImpl

---
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -0,0 +1,10 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.RoleDTO;
+
+import java.util.List;
+
+public interface RoleService {
+    List<RoleDTO> listAllRoles();
+
+}
---
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -0,0 +1,30 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
+import com.cydeo.repository.RoleRepository;
+import com.cydeo.service.RoleService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
+
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
+        this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
+    }
+
+    @Override
+    public List<RoleDTO> listAllRoles() {
+        List<Role> roleList = roleRepository.findAll();
+        return roleList.stream().map(roleMapper::convertToDto).collect(Collectors.toList());
+    }
+
+}
