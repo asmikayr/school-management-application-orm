3fd6bdfe7315f1d2513faf4107a5b711f50cc97e
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-13 15:26:21 +0400

    CourseDTO and Course entity created

---
+++ src/main/java/com/cydeo/dto/CourseDTO.java
@@ -0,0 +1,30 @@
+package com.cydeo.dto;
+
+import com.cydeo.entity.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public class CourseDTO {
+
+    @NotBlank(message = "Course name is required field")
+    @Size(max = 40, min = 2, message = "Course name must be between 2 and 40 characters long")
+    @Pattern(regexp = "([A-Za-z]\\w(?:\\s[A-Za-z]\\w*)*)", message = "Just use alphabetic characters with spaces")
+    private String name;
+
+    @NotBlank(message = "Course description is required field")
+    @Size(max = 100, min = 5, message = "Course description must be between 5 and 100 characters long")
+    private String description;
+
+    @NotNull
+    private LocalDate startDate;
+    @NotNull
+    private LocalDate endDate;
+
+    @NotNull(message = "Select a course manager")
+    private User courseManager;
+
+}
