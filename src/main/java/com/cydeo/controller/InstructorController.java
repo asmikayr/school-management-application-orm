3e74c2ae72e660ccaffcd542d8617e1e22158cbb
Asmik <jasmin_ayr@mail.ru>
2024-06-27 19:47:54 +0200

    UserStory-20 done

--- src/main/java/com/cydeo/controller/InstructorController.java
+++ src/main/java/com/cydeo/controller/InstructorController.java
@@ -1,22 +1,28 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.InstructorAssessmentDTO;
+import com.cydeo.service.InstructorAssessmentService;
 import com.cydeo.service.LessonStudentService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/instructor")
+@RequestMapping("/instructor/students")
 public class InstructorController {
 
     private final LessonStudentService lessonStudentService;
+    private final InstructorAssessmentService instructorAssessmentService;
 
-    public InstructorController(LessonStudentService lessonStudentService) {
+    public InstructorController(LessonStudentService lessonStudentService, InstructorAssessmentService instructorAssessmentService) {
         this.lessonStudentService = lessonStudentService;
+        this.instructorAssessmentService = instructorAssessmentService;
     }
 
 
-    @GetMapping("/students")
+    @GetMapping
     public String instructorPage(Model model){
 
         model.addAttribute("studentLessons", lessonStudentService.findStudentsByInstructorId(4L));
@@ -24,26 +30,24 @@ public class InstructorController {
         return "/instructor/general-assessment";
     }
 
-//    @GetMapping("/students/{email}/{id}")
-//    public String assessStudentPage(Model model, @PathVariable String email, @PathVariable Long id){
+//    @GetMapping("/{lessonStudentId}")
+//    public String assessStudentPage(Model model, @PathVariable("lessonStudentId") Long lessonStudentId){
 //
-//        model.addAttribute("instructorAssessment", new InstructorAssessment());
-//        model.addAttribute("student", studentService.findById(email));
-//        model.addAttribute("lesson", lessonService.findById(id));
+//        model.addAttribute("assessment", new InstructorAssessmentDTO());
+//        model.addAttribute("lessonStudent", lessonStudentService.findById(lessonStudentId));
+//        model.addAttribute("grades", instructorAssessmentService.findGradesByLessonStudentId(lessonStudentId));
 //
 //        return "instructor/assess-student";
 //    }
 //
-//    @PostMapping("/students/{email}/{id}")
-//    public String assessStudent(@Valid @ModelAttribute("instructorAssessment") InstructorAssessment instructorAssessment, BindingResult bindingResult, Model model, @PathVariable String email, @PathVariable Long id){
-//        Student student = studentService.findById(email);
-//        Lesson lesson = lessonService.findById(id);
+//    @PostMapping("/{lessonStudentId}")
+//    public String assessStudent(@Valid @ModelAttribute("instructorAssessment") InstructorAssessmentDTO instructorAssessment, BindingResult bindingResult, Model model, @PathVariable("lessonStudentId") Long lessonStudentId){
+//
 //        if (bindingResult.hasErrors()){
-//            model.addAttribute("student", student);
-//            model.addAttribute("lesson", lesson);
+//            model.addAttribute("lessonStudent", lessonStudentService.findById(lessonStudentId));
 //            return "/instructor/assess-student";
 //        }
-//        studentService.assessStudent(instructorAssessment, student.getEmail(), lesson);
+//        lessonStudentService.assessStudent(instructorAssessment, lessonStudentId);
 //        return "redirect:/instructor/students";
 //    }
 }
--- src/main/java/com/cydeo/service/impl/LessonStudentServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/LessonStudentServiceImpl.java
@@ -1,12 +1,16 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.InstructorAssessmentDTO;
 import com.cydeo.dto.LessonStudentDTO;
+import com.cydeo.entity.InstructorAssessment;
 import com.cydeo.entity.LessonStudent;
 import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repository.InstructorAssessmentRepository;
 import com.cydeo.repository.LessonStudentRepository;
 import com.cydeo.service.LessonStudentService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,10 +18,12 @@ import java.util.stream.Collectors;
 public class LessonStudentServiceImpl implements LessonStudentService {
 
     private final LessonStudentRepository lessonStudentRepository;
+    private final InstructorAssessmentRepository instructorAssessmentRepository;
     private final MapperUtil mapperUtil;
 
-    public LessonStudentServiceImpl(LessonStudentRepository lessonStudentRepository, MapperUtil mapperUtil) {
+    public LessonStudentServiceImpl(LessonStudentRepository lessonStudentRepository, InstructorAssessmentRepository instructorAssessmentRepository, MapperUtil mapperUtil) {
         this.lessonStudentRepository = lessonStudentRepository;
+        this.instructorAssessmentRepository = instructorAssessmentRepository;
         this.mapperUtil = mapperUtil;
     }
 
@@ -32,4 +38,13 @@ public class LessonStudentServiceImpl implements LessonStudentService {
     public LessonStudentDTO findById(Long id) {
         return mapperUtil.convert(lessonStudentRepository.findById(id), LessonStudentDTO.class);
     }
+
+//    @Override
+//    public void assessStudent(InstructorAssessmentDTO instructorAssessment, Long lessonStudentId) {
+//        instructorAssessment.setGradeDate(LocalDate.now());
+//        LessonStudentDTO lessonStudent = findById(lessonStudentId);
+//        instructorAssessment.setLessonStudent(lessonStudent);
+//
+//        instructorAssessmentRepository.save(mapperUtil.convert(instructorAssessment, InstructorAssessment.class));
+//    }
 }
