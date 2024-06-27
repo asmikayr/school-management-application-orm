ac7bd356be9bbbd0765349ed9ccb289393ca5bb7
Asmik <jasmin_ayr@mail.ru>
2024-06-27 15:57:17 +0200

    LessonStudentService: findAllByInstructor finished

---
+++ src/main/java/com/cydeo/service/LessonStudentService.java
@@ -0,0 +1,10 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.LessonStudentDTO;
+
+import java.util.List;
+
+public interface LessonStudentService {
+
+    List<LessonStudentDTO > findStudentsByInstructorId(Long id);
+}
---
+++ src/main/java/com/cydeo/service/impl/LessonStudentServiceImpl.java
@@ -0,0 +1,30 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.LessonStudentDTO;
+import com.cydeo.entity.LessonStudent;
+import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repository.LessonStudentRepository;
+import com.cydeo.service.LessonStudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class LessonStudentServiceImpl implements LessonStudentService {
+
+    private final LessonStudentRepository lessonStudentRepository;
+    private final MapperUtil mapperUtil;
+
+    public LessonStudentServiceImpl(LessonStudentRepository lessonStudentRepository, MapperUtil mapperUtil) {
+        this.lessonStudentRepository = lessonStudentRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+    @Override
+    public List<LessonStudentDTO> findStudentsByInstructorId(Long id) {
+
+        List<LessonStudent> students = lessonStudentRepository.findAllByLessonInstructorId(id);
+        return students.stream().map(s-> mapperUtil.convert(s, LessonStudentDTO.class)).collect(Collectors.toList());
+    }
+}
