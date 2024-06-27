c1df98ddbc9ee49ffba9ec8d6bad1176361210ee
Asmik <jasmin_ayr@mail.ru>
2024-06-27 15:56:49 +0200

    LessonStudentRepository: findAllByInstructor finished

--- src/main/java/com/cydeo/repository/LessonStudentRepository.java
+++ src/main/java/com/cydeo/repository/LessonStudentRepository.java
@@ -3,5 +3,9 @@ package com.cydeo.repository;
 import com.cydeo.entity.LessonStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LessonStudentRepository extends JpaRepository<LessonStudent, Long> {
+
+    List<LessonStudent> findAllByLessonInstructorId(Long id);
 }
